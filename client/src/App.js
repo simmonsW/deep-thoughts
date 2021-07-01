62ec54e08c85fa06353847119489979a680613af
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-01 11:40:23 -0700

    create and implement authentication functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@ap
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { setContext } from '@apollo/client/link/context';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -18,8 +20,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) =>{
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
